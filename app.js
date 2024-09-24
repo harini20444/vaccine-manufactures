82b88fb0bf8aee04eb977f4c72f7375769568418
harini20444 <22eg107b44@anurag.edu.in>
2024-09-24 21:35:35 +0530

    port

--- app.js
+++ app.js
@@ -43,5 +43,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+app.listen(1000, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 module.exports = app;
