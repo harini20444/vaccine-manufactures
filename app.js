15e6f0dca26d166400a71c0f82dd6719da98122f
harini20444 <22eg107b44@anurag.edu.in>
2024-09-25 00:25:09 +0530

    start

--- app.js
+++ app.js
@@ -9,11 +9,22 @@ var indexRouter = require('./app_server/routes/index');
 // var usersRouter = require('./app_server/routes/users');
 const apiRouter = require('./app_api/routes/index');
 
-var app = express();
 
 // view engine setup
 // app.set('views', path.join(__dirname,'app_server', 'views'));
 // app.set('view engine', 'jade');
+
+const mongoose = require("mongoose");
+const dbURI = "mongodb+srv://22eg107b44:abc@cluster0.lkgsi.mongodb.net/app?retryWrites=true&w=majority&appName=Cluster0";
+
+var app = express();
+
+// For nodemon restarts
+process.once("SIGUSR2", () => {
+  gracefulShutdown("nodemon restart", () => {
+    process.kill(process.pid, "SIGUSR2");
+  });
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -43,8 +54,22 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-// app.listen(1000, () => {
-//   console.log(`app listening on port ${1000}`)
-// })
+
+mongoose.connect(dbURI, { useNewUrlParser: true });
+mongoose.connection.on("connected", () => {
+  console.log(`Mongoose connected to ${dbURI}`);
+  app.listen(1000, () => {
+    console.log(`app listening on port ${1000}`)
+  })
+});
+mongoose.connection.on("error", (err) => {
+  console.log(`Mongoose connection error: ${err}`);
+});
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongoose disconnected");
+});
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close();
+};
 
 module.exports = app;
--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,24 +1,4 @@
-const mongoose = require("mongoose");
-const dbURI = "mongodb+srv://22eg107b44:abc@cluster0.lkgsi.mongodb.net/app?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(dbURI, { useNewUrlParser: true });
-mongoose.connection.on("connected", () => {
-  console.log(`Mongoose connected to ${dbURI}`);
-});
-mongoose.connection.on("error", (err) => {
-  console.log(`Mongoose connection error: ${err}`);
-});
-mongoose.connection.on("disconnected", () => {
-  console.log("Mongoose disconnected");
-});
-const gracefulShutdown = (msg, callback) => {
-  mongoose.connection.close();
-};
-// For nodemon restarts
-process.once("SIGUSR2", () => {
-  gracefulShutdown("nodemon restart", () => {
-    process.kill(process.pid, "SIGUSR2");
-  });
-});
+
 // For app termination
 process.on("SIGINT", () => {
   gracefulShutdown("app termination", () => {
