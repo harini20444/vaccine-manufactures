65d90d380260eb00ec8145d23229d7be29315c32
harini20444 <22eg107b44@anurag.edu.in>
2024-09-25 00:11:55 +0530

    about

---
+++ app_server/controllers/others.js
@@ -0,0 +1,11 @@
+/* GET 'about' page */
+const about = function (req, res) {
+    res.render("generic-text", {
+      title: "About",
+      content:
+        "India is a major player in the global vaccine industry, home to several prominent vaccine manufacturers. These companies are known for producing a range of vaccines for various diseases, including those for COVID-19, polio, hepatitis, and more.",
+    });
+  };
+  module.exports = {
+    about,
+  };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrlLocations =
 require('../controllers/locations');
-// const ctrlOthers = require('../controllers/others');
+const ctrlOthers = require('../controllers/others');
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
 
@@ -12,5 +12,5 @@ router.get("/location/:_id",ctrlLocations.getLocation)
 // router.get('/location/BiologicalE',ctrlLocations.locationInfoBiologicalE);
 // router.get('/location/IndianImmunologicals',ctrlLocations.locationInfoIndianImmunologicals);
 /* Other pages */
-// router.get('/about', ctrlOthers.about);
+router.get('/about', ctrlOthers.about);
 module.exports = router;
